3ba0401f370f7bd641b193da74205962068b2b1d
srgmrtnvqr <srg.mrtn.vqr@gmail.com>
2024-08-01 14:31:37 +0200

    Pantalla secundaria para la bienvenida.

--- app/src/main/java/com/srgmrtnvqr/datosdiarios/MainActivity.kt
+++ app/src/main/java/com/srgmrtnvqr/datosdiarios/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.srgmrtnvqr.datosdiarios
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val SHARED_PREFS = "shared_prefs"
+        const val USUARIO_KEY = "usuario_key"
+        const val CONTRASEÑA_KEY = "contraseña_key"
+    }
+
+    private lateinit var sharedpreferences: SharedPreferences
+    private var usuario: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        sharedpreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE)
+        usuario = sharedpreferences.getString(USUARIO_KEY, null)
+
+        val welcomeTV = findViewById<TextView>(R.id.idTVWelcome)
+        welcomeTV.text = "Bienvenido $usuario"
+        val logoutBtn = findViewById<Button>(R.id.idBtnLogout)
+        logoutBtn.setOnClickListener {
+            val editor = sharedpreferences.edit()
+            editor.clear()
+            editor.apply()
+
+            val i = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(i)
+            finish()
         }
     }
 }
